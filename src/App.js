50dd64f65b619104fb12ca5d36b5c47f603b3cc0
Scott Ross <scott.william.ross2@gmail.com>
2022-05-24 12:37:23 -0400

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
+    <div className="calc-grid">
+      <div className="output">
+        <div className="prev-op">
 
+        </div>
+        <div className="curr-op">
+
+        </div>
+      </div>
+
+      <button className="span-two">ac</button>
+      <button>del</button>
+      <button>/</button>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+
+      <button className="span-two">0</button>
+      <button>.</button>
+      <button>=</button>
     </div>
   );
 }
